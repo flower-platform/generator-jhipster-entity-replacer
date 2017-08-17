2f6638e46bf037f437a541f3a9589139f17592f3
anca barbu <anca.barbu@crispico.ro>
2017-08-17 12:11:54 +0300

    commit install mechanism

--- generators/app/index.js
+++ generators/app/index.js
@@ -138,6 +138,36 @@ module.exports = JhipsterGenerator.extend({
             this.log(`${chalk.red.bold('WARN!')} Could not register as a jhipster entity post creation hook...\n`);
         }
 	},
+	
+   install() {
+        let logMsg =
+            `To install your dependencies manually, run: ${chalk.yellow.bold(`${this.clientPackageManager} install`)}`;
+
+        if (this.clientFramework === 'angular1') {
+            logMsg =
+                `To install your dependencies manually, run: ${chalk.yellow.bold(`${this.clientPackageManager} install & bower install`)}`;
+        }
+        const injectDependenciesAndConstants = (err) => {
+            if (err) {
+                this.warning('Install of dependencies failed!');
+                this.log(logMsg);
+            } else if (this.clientFramework === 'angular1') {
+                this.spawnCommand('gulp', ['install']);
+            }
+        };
+        const installConfig = {
+            bower: this.clientFramework === 'angular1',
+            npm: this.clientPackageManager !== 'yarn',
+            yarn: this.clientPackageManager === 'yarn',
+            callback: injectDependenciesAndConstants
+        };
+        if (this.options['skip-install']) {
+            this.log(logMsg);
+        } else {
+            this.installDependencies(installConfig);
+        }
+    },
+
     end() {
 		ncp(`${javaDir}domain/`, `../${javaDir}domain/`, {"clobber": true}, function (err) {
 			if (err) {
